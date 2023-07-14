09ddadee89f7580212146986cc7a61a6a2857730
Russanandres <71694426+Russanandres@users.noreply.github.com>
2023-07-15 00:02:17 +0300

    Add files via upload

--- tilte.sh
+++ tilte.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 IFS=$'\n'
-err=0;total=0;renamed=0
-ext=mp3
+err="0";total="0";renamed="0";sim="0"
+ext="mp3"
 # source $USER/.config/tilte.conf
 
 if [ -z $1 ]; then echo "Nothing to do"; exit; else way="$1";fi
-way=$(readlink -f $1 &> /dev/null)
+way=$(readlink -f $1)
 
 Green='\033[0;32m'
 BIRed='\033[1;91m'
@@ -14,7 +14,7 @@ BIBlue='\033[1;94m'
 No_color='\033[0m'
 
 function exitscr(){ clear;echo "Tilte. By Russanandres";date;exit
- }
+}
 
 # Okay, so "reduction - meaning"
 # ar - artist
@@ -24,20 +24,24 @@ function exitscr(){ clear;echo "Tilte. By Russanandres";date;exit
 # an - song number
 # sn - side number
 
-
 function helpscr(){
 echo "tilte.sh. By Russanandres.
 Usage: tilte.sh [path] [option]
 Availiable options:
-    -v | --verbose      >>  Show full process log
-    -f | --no-verbose   >>  Show minimal log
-    -h | --help         >>  Show help manual"; exit
- }
+    -v | --verbose      >>  Show more output
+    -f | --no-verbose   >>  Show less output
+    -s | --simulate     >>  Simulate the process
+    -h | --help         >>  Show help manual
+    -r | --recurse      >>  Rename through subdirectories"; exit
+}
+
 while [ "$1" != "" ]; do
     case $1 in
         -v | --verbose) verbose="1";;
-        -f | -s | --no-verbose | --fast | --silent ) fast="1";;
+        -f | --no-verbose | --fast | --silent ) fast="1";;
+        -s | --simulate ) sim="1";;
         -h | --help ) helpscr;;
+        -r | --recurse ) rec="1";;
     esac
     shift
 done
@@ -60,12 +64,14 @@ ext=${file##*.}
 artist=$(ffprobe -loglevel error -show_entries format_tags=artist -of default=noprint_wrappers=1:nokey=1 $file)
 title=$(ffprobe -loglevel error -show_entries format_tags=title -of default=noprint_wrappers=1:nokey=1 $file)
 echo -e "${Green} Processing $file. Renamed to${BIBlue} $artist - $title.$ext${No_color}"
-if [[ ! -z "$title" ]] && [[ ! -z "$artist" ]] && [[ ! -z "$ext" ]]; then mv -f "$file" "$way"/"$artist - $title.$ext"; fi
+if [[ ! -z "$title" ]] && [[ ! -z "$artist" ]] && [[ ! -z "$ext" ]] && [ "$sim" == "0" ]; then mv -f "$file" "$way"/"$artist - $title.$ext"; fi
 if [ "$?" == 1 ]; then let err=$err+1; else let renamed=$renamed+1; fi
 let total=$total+1
 trap "break" SIGINT
 done
 }
+
+
 
 
 function verbose(){ clear
@@ -79,7 +85,7 @@ artist=$(ffprobe -loglevel error -show_entries format_tags=artist -of default=no
 echo -e "Artist is:${BIBlue} $artist${No_color}"
 title=$(ffprobe -loglevel error -show_entries format_tags=title -of default=noprint_wrappers=1:nokey=1 $file)
 echo -e "Title is:${BIBlue} $title${No_color}"
-if [[ ! -z "$title" ]] && [[ ! -z "$artist" ]] && [[ ! -z "$ext" ]]; then mv -v -f "$file" "$way"/"$artist - $title.$ext"; fi
+if [[ ! -z "$title" ]] && [[ ! -z "$artist" ]] && [[ ! -z "$ext" ]] && [ "$sim" == "0" ]; then mv -v -f "$file" "$way"/"$artist - $title.$ext"; fi
 if [ "$?" == 1 ]; then let err=$err+1; else let renamed=$renamed+1; fi
 echo -e "Renamed to${BIBlue} $artist - $title.$ext${No_color}"
 let total=$total+1
@@ -87,17 +93,45 @@ trap "echo; break" SIGINT
 done
 }
 
+
+
+
+function rec(){ clear
+for file in $(find $way); do
+echo -e "${Green}------------------------------------------------------${No_color}"
+echo -e "Processing${BIBlue} $file${No_color}"
+unset title; unset artist
+ext=${file##*.}
+echo -e "Extension is:${BIBlue} $ext${No_color}"
+artist=$(ffprobe -loglevel error -show_entries format_tags=artist -of default=noprint_wrappers=1:nokey=1 $file)
+echo -e "Artist is:${BIBlue} $artist${No_color}"
+title=$(ffprobe -loglevel error -show_entries format_tags=title -of default=noprint_wrappers=1:nokey=1 $file)
+echo -e "Title is:${BIBlue} $title${No_color}"
+if [[ ! -z "$title" ]] && [[ ! -z "$artist" ]] && [[ ! -z "$ext" ]] && [ "$sim" == "0" ]; then mv -v -f "$file" "$way"/"$artist - $title.$ext"; fi
+if [ "$?" == 1 ]; then let err=$err+1; else let renamed=$renamed+1; fi
+echo -e "Renamed to${BIBlue} $artist - $title.$ext${No_color}"
+let total=$total+1
+trap "echo; break" SIGINT
+done
+}
+
+
+
+
 function fast(){
 renamed=X;err=X
 for file in $way/* ; do
 ext=${file##*.};mv "$file" $way/"$(ffprobe -loglevel error -show_entries format_tags=artist -of default=noprint_wrappers=1:nokey=1 $file) - $(ffprobe -loglevel error -show_entries format_tags=title -of default=noprint_wrappers=1:nokey=1 $file).$ext"
 let total=$total+1
-trap "break" SIGINT
+trap "echo;break" SIGINT
 done
 }
 
 if [ "$fast" == "1" ]; then fast
-elif [ "$verbose" == "1" ]; then verbose; else long; fi
+elif [ "$rec" == "1" ]; then rec
+elif [ "$verbose" == "1" ]; then verbose
+else long
+fi
 
 ;;
 * ) exitscr;;
